109219104e8d142cc2687c0f7dbb905193e8a234
iyonaga <yiyonaga@gmail.com>
2018-10-23 19:43:42 +0900

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+class HtmlWebpackAdditionalTemplatePlugin {
+  apply(compiler) {
+    const plugins = compiler.options.plugins;
+
+    plugins.map(plugin => {
+      if (
+        plugin.constructor.name === 'HtmlWebpackPlugin' &&
+        plugin.options.additionalTemplate
+      ) {
+        const commonOptions = Object.assign({}, plugin.options);
+        delete commonOptions.additionalTemplate;
+
+        plugin.options.additionalTemplate.map(data => {
+          compiler.options.plugins.push(
+            new HtmlWebpackPlugin(
+              Object.assign({}, commonOptions, {
+                filename: data.filename,
+                template: data.template
+              })
+            )
+          );
+        });
+
+        delete plugin.options.additionalTemplate;
+      }
+    });
+  }
+}
+
+module.exports = HtmlWebpackAdditionalTemplatePlugin;
